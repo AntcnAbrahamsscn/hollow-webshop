e644969c20df7f162f2718b4c7fae12063419730
Anton Abrahamsson <abrahamsson37@gmail.com>
2024-04-23 17:14:05 +0200

    Added firestore

---
+++ src/data/firestore.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCudVkmk3mQya0HTUeUBIuZGpwpOwIkrq8",
+  authDomain: "hollow-webbshop.firebaseapp.com",
+  projectId: "hollow-webbshop",
+  storageBucket: "hollow-webbshop.appspot.com",
+  messagingSenderId: "408169781982",
+  appId: "1:408169781982:web:873a7ffc9f3f5e08f2804f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
